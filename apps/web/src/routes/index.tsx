43761bd4cf12bdd52f104b5dd14356f22619c872
Mat Manna <matmanna@outlook.com>
2026-07-15 22:55:05 -0400

    feat(web): beautify/descriptify lander

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -17,6 +17,38 @@ function HomeComponent() {
         <div className="mx-auto mt-[-100px] max-w-6xl">
           <AsciiMotionAnimation showControls={false} autoPlay={false} />
         </div>
+        <div className="mx-auto flex w-full max-w-3xl flex-col gap-2 font-mono text-sm">
+          <p className="text-md">
+            <a href="/" className="text-[#ffa364] hover:underline">
+              🍊 (marmalade)
+            </a>{" "}
+            is a permissionful api access layer for{" "}
+            <a
+              href="https://letsjelly.com"
+              className="text-[#e2366a] hover:underline"
+            >
+              🍓 (jelly)
+            </a>
+          </p>
+          <ul className="list-inside list-disc pl-6">
+            <li>
+              made for silliness, freedom, and :3 in{" "}
+              <a
+                href="https://hackclub.com"
+                className="hover:text-[#ec3750] hover:underline"
+              >
+                hack club
+              </a>
+            </li>
+            <li>
+              found something we lost? help us and{" "}
+              <a href="/keys/revoke" className="text-[#a1a1a1] hover:underline">
+                revoke
+              </a>{" "}
+              it 🥰
+            </li>
+          </ul>
+        </div>
         <section className="mx-auto w-full max-w-3xl rounded-lg border p-4">
           <h2 className="mb-2 font-medium">API Status</h2>
           <div className="flex items-center gap-2">

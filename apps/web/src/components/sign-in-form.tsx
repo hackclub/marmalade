51039f56a8f228b8cb665eb36c4b0ecdb735810a
Mat Manna <matmanna@outlook.com>
2026-07-17 21:06:03 -0400

    feat: why must thou make me suffer

--- apps/web/src/components/sign-in-form.tsx
+++ apps/web/src/components/sign-in-form.tsx
@@ -1,4 +1,13 @@
 import { Button } from "@marmalade-v2/ui/components/button";
+import { Input } from "@marmalade-v2/ui/components/input";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@marmalade-v2/ui/components/input-otp";
+import { useState } from "react";
+
 import { useNavigate } from "@tanstack/react-router";
 import { toast } from "sonner";
 
@@ -11,7 +20,9 @@ export default function SignInForm() {
     from: "/",
   });
   const { isPending, data } = authClient.useSession();
-
+  const [fixedEmail, setFixedEmail] = useState<string | null>(null);
+  const [nonFixedEmail, setNonFixedEmail] = useState<string>("");
+  const [otp, setOtp] = useState<string>("");
   if (isPending) {
     return <Loader />;
   }
@@ -26,7 +37,7 @@ export default function SignInForm() {
       <h1 className="mb-6 text-center text-3xl font-bold">
         Welcome to Marmalade
       </h1>
-      <p className="mb-8 text-center text-gray-600">
+      <p className="mb-8 text-center text-gray-400">
         Sign in with your Hack Club account to continue
       </p>
 
@@ -45,6 +56,100 @@ export default function SignInForm() {
       >
         Sign in with Hack Club
       </Button>
+      <details className="mt-4">
+        <summary className="my-4 text-white">
+          but... my jelly account email is different from my HCA
+        </summary>
+        <div className="flex flex-col gap-4">
+          <p className="text-gray-400">
+            (i guess i can let you in with email/password if you really want to,
+            but this is a hack club app so i don't know why you would want to do
+            that)
+          </p>
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={async (e) => {
+              e.preventDefault();
+              setFixedEmail(nonFixedEmail);
+
+              const { data, error } =
+                await authClient.emailOtp.sendVerificationOtp({
+                  email: nonFixedEmail, // required
+                  type: "sign-in", // required
+                });
+              if (error) {
+                setFixedEmail(null);
+                toast.error(
+                  error.message || "Failed to sign in with Email OTP",
+                );
+                return;
+              }
+            }}
+          >
+            <Input
+              disabled={!!fixedEmail}
+              value={nonFixedEmail}
+              onChange={(e) => setNonFixedEmail(e.target.value)}
+              type="email"
+              name="email"
+              placeholder="non-hca email"
+            />
+
+            <Button type="submit" className="w-full" size="lg">
+              OTP me
+            </Button>
+          </form>
+          {fixedEmail && (
+            <form
+              className="mt-2 flex flex-col items-center gap-2"
+              onSubmit={async (e) => {
+                e.preventDefault();
+
+                const { error } = await authClient.signIn.emailOtp({
+                  email: fixedEmail, // required
+                  name: fixedEmail.split("@")[0] || "",
+                  otp: otp, // required
+                });
+                if (error) {
+                  toast.error(error.message || "Failed to verify email OTP");
+                } else {
+                  navigate({ to: "/" });
+                }
+              }}
+            >
+              <InputOTP
+                name="otp"
+                maxLength={6}
+                value={otp}
+                onChange={(e) => setOtp(e)}
+              >
+                <InputOTPGroup>
+                  <InputOTPSlot index={0} />
+                  <InputOTPSlot index={1} />
+                  <InputOTPSlot index={2} />
+                </InputOTPGroup>
+                <InputOTPSeparator />
+                <InputOTPGroup>
+                  <InputOTPSlot index={3} />
+                  <InputOTPSlot index={4} />
+                  <InputOTPSlot index={5} />
+                </InputOTPGroup>
+              </InputOTP>
+              <Button type="submit" className="w-full" size="lg">
+                verify OTP
+              </Button>
+              <Button
+                variant="ghost"
+                className="w-full"
+                size="lg"
+                onClick={() => setFixedEmail(null)}
+              >
+                i typed my email wrong
+              </Button>
+            </form>
+          )}
+        </div>
+      </details>
     </div>
   );
 }
--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -4,12 +4,46 @@ import * as schema from "@marmalade-v2/db/schema/auth";
 import { env } from "@marmalade-v2/env/server";
 import { betterAuth, type BetterAuthOptions } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { genericOAuth } from "better-auth/plugins";
+import { emailOTP, genericOAuth } from "better-auth/plugins";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
+import nodemailer from "nodemailer";
 
 export type AuthOptions = {
   databaseHooks?: BetterAuthOptions["databaseHooks"];
 };
+
+async function sendEmailVerificationOTP({
+  email,
+  otp,
+}: {
+  email: string;
+  otp: string;
+}) {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    secure: true,
+    port: 465,
+    auth: {
+      user: env.EMAIL_USER,
+      pass: env.EMAIL_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+  const mailOptions = {
+    from: env.EMAIL_USER,
+    to: email,
+    subject: "Marmalade One Time Password",
+    html: `<div><p>*sigh* i guess you're really serious about making my life harder, so here's your one-time code vro: </p> <h1>${otp}</h1>
+    <i>PS: in case it wasn't clear, i'd appreciate if you considered switching your HCA over to this address since i don't want to deal with sending email OTPs or maintaining two different authentication methods, kthxbye</i></div>`,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+  } finally {
+    transporter.close();
+  }
+}
 
 export function createAuth(options?: AuthOptions) {
   const db = createDb();
@@ -46,6 +80,11 @@ export function createAuth(options?: AuthOptions) {
               },
             ]
           : [],
+      }),
+      emailOTP({
+        async sendVerificationOTP({ email, otp, type }) {
+          await sendEmailVerificationOTP({ email, otp });
+        },
       }),
     ],
   });
